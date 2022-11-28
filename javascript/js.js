e5e756be7753f439e3d724de46bd66f328776a81
Arun4Rajbhar <89437068+Arun4Rajbhar@users.noreply.github.com>
2022-11-28 02:24:50 -0800

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,12 @@
+
+var a='hello world';
+var b=67;
+var c=435;
+console.log(b+c);
+
+document.write(b-c,"<br>");
+console.log(a);
+document.write("follow denerian day now");
+let a=78;
+const d=2347;
+let a=56;
